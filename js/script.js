024d21cef02e93e1248e64685c429eb9f9e99083
slavikovab <barbora.slavikova@student.spsmb.cz>
2024-04-24 21:05:42 +0200

    Responsivity done

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const openNav = document.getElementById("openNav");
+const closeNav = document.getElementById("closeNav");
+const mobileNavbar = document.getElementById("mNav");
+
+const btns1 = document.getElementById("btns1");
+const btns2 = document.getElementById("btns2");
+const btns3 = document.getElementById("btns3");
+const btns4 = document.getElementById("btns4");
+const btns5 = document.getElementById("btns5");
+const btns6 = document.getElementById("btns6");
+
+let visible = false;
+
+btns2.onclick = () => {
+    if(visible){
+        mobileNavbar.style.display = "none";
+        visible = false
+    }
+}
+btns3.onclick = () => {
+    if(visible){
+        mobileNavbar.style.display = "none";
+        visible = false
+    }
+}
+btns4.onclick = () => {
+    if(visible){
+        mobileNavbar.style.display = "none";
+        visible = false
+    }
+}
+btns5.onclick = () => {
+    if(visible){
+        mobileNavbar.style.display = "none";
+        visible = false
+    }
+}
+btns6.onclick = () => {
+    if(visible){
+        mobileNavbar.style.display = "none";
+        visible = false
+    }
+}
+openNav.onclick = () => {
+    if (!visible){
+        mobileNavbar.style.display = "grid";
+        mobileNavbar.classList.add("open-animation");
+        visible = true;
+    }
+}
+closeNav.onclick = () => {
+    if(visible){
+        mobileNavbar.style.display = "none";
+        visible = false
+    }
+}
