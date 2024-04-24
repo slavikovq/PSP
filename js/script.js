b583f15539ea67c78e287489fe4cee809859feb3
slavikovab <barbora.slavikova@student.spsmb.cz>
2024-04-24 21:31:23 +0200

    meow

--- js/script.js
+++ js/script.js
@@ -54,3 +54,18 @@ closeNav.onclick = () => {
         visible = false
     }
 }
+
+window.onscroll = () => {
+    scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+scrollBtn.onclick = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
